344156a01d1f13f52886daa9b3a0ede7b11a3a5f
Claudiu Gombos <claudiu.gombos@thinslices.com>
2022-12-24 21:37:46 +0200

    added executeQuery

--- src/api/api.js
+++ src/api/api.js
@@ -1,61 +1,75 @@
-import { useQuery } from '@apollo/client';
-import { memoryStorage } from '../localStorage/memoryStorage';
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { memoryStorage } from "../localStorage/memoryStorage";
+import { useApolloClient } from "@apollo/client";
 
 //export declare function useQuery<TData = any, TVariables = OperationVariables>(query: DocumentNode | TypedDocumentNode<TData, TVariables>, options?: QueryHookOptions<TData, TVariables>): QueryResult<TData, TVariables>;
 
-function useExecuteQuery(
-  name,
-  query,
-  variables
-) {
-//   const memoryInfo = memoryStorage.get(name);
-//   if (memoryInfo) {
-//     return memoryInfo;
-//   }
-//   memoryStorage.set(name, {});
-
-  const { data, loading, error } = variables
-    ? useQuery(query, { variables })
-    : useQuery(query);
-
-  if (error) {
-    console.log(error);
+const isPromise = (p) => {
+  if (typeof p === "object" && typeof p.then === "function") {
+    return true;
   }
+  return false;
+};
 
-  const response = { data, loading, error };
-  if(!response.loading){
-    memoryStorage.set(name, response);
-  }
+const useExecuteQuery = (name, query, variables) => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const [data, setData] = useState(null);
 
-  return response;
-}
-
-function executeQuery(
-    name,
+  const request = {
     query,
-    variables
-  ) {
+  };
+  if (variables) {
+    request.variables = variables;
+  }
+
+  const client = useApolloClient();
+
+  useEffect(() => {
     const memoryInfo = memoryStorage.get(name);
     if (memoryInfo) {
-        debugger;
-      return memoryInfo;
+      if (!isPromise(memoryInfo)) {
+        setData(memoryInfo);
+        setIsLoading(false);
+        return;
+      }
+      memoryInfo.then((response) => {
+        const dataResponse = response.data.data;
+        setData(dataResponse);
+        setIsLoading(false);
+      });
+      return;
     }
-  //   memoryStorage.set(name, {});
-  
-    const { data, loading, error } = variables
-      ? useQuery(query, { variables })
-      : useQuery(query);
-  
-    if (error) {
-      console.log(error);
-    }
-  
-    const response = { data, loading, error };
-    if(!response.loading){
-      memoryStorage.set(name, response);
-    }
-  
-    return response;
-  }
 
-export { useExecuteQuery, executeQuery };
+    const promise = axios.post(client.link.options.uri, request, {
+      "Content-Type": "application/json",
+      Authorization: "JWT fefege...",
+    });
+    memoryStorage.set(name, promise);
+
+    promise
+      .then((response) => {
+        const dataResponse = response.data.data;
+        setData(dataResponse);
+        setIsLoading(false);
+
+        const apiResponse = {
+          data: dataResponse,
+          loading: false,
+          error: null,
+        };
+        memoryStorage.set(name, apiResponse);
+      })
+      .catch((error) => {
+        // debugger;
+        console.log(error);
+        setError(error);
+      });
+  }, []);
+
+  return { isLoading, data, error };
+};
+
+export { useExecuteQuery };
+// export default useExecuteQuery;
--- src/users.js
+++ src/users.js
@@ -1,7 +1,20 @@
-const { useQuery, gql } = require("@apollo/client");
-const { useExecuteQuery } = require("./api/api");
+import { useEffect, useRef } from "react";
+import React from "react";
+// const useExecuteQuery  = require("./api/api");
+import { useExecuteQuery, useAsyncHookWithLoading1 } from "./api/api";
+//https://codesandbox.io/s/react-async-custom-hook-example-with-and-without-loading-zhbjrl?file=/src/useAsyncHookWithoutLoading.js
 
-const USERS_QUERY = gql`
+const { useQuery, gql } = require("@apollo/client");
+
+const GQL_USERS_QUERY = gql`
+  query ExampleQuery {
+    allUsers {
+      id
+    }
+  }
+`;
+
+const USERS_QUERY = `
 query ExampleQuery {
   allUsers {
     id
@@ -10,18 +23,80 @@ query ExampleQuery {
 `;
 
 const Users = () => {
+  // const users = useQuery(GQL_USERS_QUERY);
 
-    const users = useQuery(USERS_QUERY);
-    const { loading, error, data } = useQuery(USERS_QUERY);
-    console.log(data)
+  // const { loading, error, data } = useQuery(GQL_USERS_QUERY);
+  // console.log(data);
 
-    // const makeCall = () => {
-    //     const { loading, error, data } = useQuery(USERS_QUERY);
-    // }
+  // const [list, setMyList] = React.useState([]);
+  // const myRef = useRef(0);
 
-    // return (
-    //     <Button onClick={makeCall}></Button>
-    // )
-}
+  const {
+    error,
+    isLoading,
+    data,
+  } = useExecuteQuery("users", USERS_QUERY, null);
+  console.log({ error, isLoading, data });
+
+  // const {
+  //   error:e1,
+  //   isLoading:l1,
+  //   data:d1,
+  // } = useExecuteQuery("users", USERS_QUERY, null);
+  // console.log({ e1, l1, d1 });
+
+  const x = useExecuteQuery("users", USERS_QUERY, null);
+  console.log('x', x);
+
+  // useEffect(() => {
+  //   console.log('useEffect');
+  //   setTimeout(() => {
+  //     console.log('setTimeout');
+  //     const {
+  //       error:e2,
+  //       isLoading:l2,
+  //       data:d2,
+  //     // eslint-disable-next-line react-hooks/rules-of-hooks
+  //     } = useExecuteQuery("users", USERS_QUERY, null);
+  //     console.log({ e2, l2, d2 });
+  //   }, 2000);
+
+  // }, []);
+
+
+  // (async () => {
+  //   // eslint-disable-next-line react-hooks/rules-of-hooks
+  //   const response =  await useExecuteQuery("users", USERS_QUERY, null);
+  //   // debugger;
+  //   // setMyList(response.data.allUsers);
+  //   console.log(response);
+
+  // })();
+
+  
+
+  return (
+    <>
+      {isLoading ? (
+        <p>useAsyncHookWithLoading is loading a message...</p>
+      ) : (
+        <p>{error}</p>
+      )}
+
+      {/* {console.log(data1)}
+      {
+        
+         data1 && data1?.allUsers.map(({ id }) => {
+            return <div key={id}>{id}</div>;
+          })
+      } */}
+
+      {data &&
+        data?.allUsers.map(({ id }) => {
+          return <div key={id}>{id}</div>;
+        })}
+    </>
+  );
+};
 
 export default Users;
